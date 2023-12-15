ae5f7aac7ed0988abcc75b51559dd256e5f8d8c9
RaulGarCan <10303@cifpceuta.es>
2023-12-15 21:16:00 +0100

    Initial Commit

--- app/src/main/java/com/cifpceuta/appplanifica/ManejadorSQLite.java
+++ app/src/main/java/com/cifpceuta/appplanifica/ManejadorSQLite.java
@@ -1,7 +1,25 @@
 package com.cifpceuta.appplanifica;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class ManejadorSQLite extends SQLiteOpenHelper {
+import androidx.annotation.Nullable;
 
+public class ManejadorSQLite extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "nombreBD.db";
+    public ManejadorSQLite(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
